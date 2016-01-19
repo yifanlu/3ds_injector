e1f836f2775b24e21a0d3d458abee75c5a18410c
Yifan Lu <me@yifanlu.com>
2016-01-19 00:20:02 -0800

    Implemented LoadProcess except load_code

--- loader.c
+++ loader.c
@@ -1,15 +1,142 @@
 #include <stdlib.h>
 #include <3ds.h>
+#include "exheader.h"
 
 #define MAX_SESSIONS 1
 
+typedef struct
+{
+  u32 text_addr;
+  u32 text_size;
+  u32 ro_addr;
+  u32 ro_size;
+  u32 data_addr;
+  u32 data_size;
+  u32 total_size;
+} prog_addrs_t;
+
 static Handle g_handles[MAX_SESSIONS+2];
 static int g_active_handles;
+static u64 g_cached_prog_handle;
+static exheader_header g_exheader;
+static char g_ret_buf[1024];
+
+static int allocate_shared_mem(prog_addrs_t *shared, prog_addrs_t *vaddr, int flags)
+{
+  u32 dummy;
+
+  memcpy(shared, vaddr, sizeof(prog_addrs_t));
+  shared->text_addr = 0x10000000;
+  shared->ro_addr = shared->text_addr + (shared->text_size << 12);
+  shared->data_addr = shared->ro_addr + (shared->ro_size << 12);
+  return svcControlMemory(&dummy, shared->text_addr, 0, shared->total_size << 12, flags & 0xF00 | MEMOP_ALLOC, MEMPERM_READ | MEMPERM_WRITE);
+}
+
+static int load_code(prog_addrs_t *shared, u64 prog_handle, int is_compressed)
+{
+  
+}
+
+static int loader_LoadProcess(Handle &process, u64 prog_handle)
+{
+  int res;
+  int count;
+  u32 flags;
+  u32 desc;
+  prog_addrs_t shared_addr;
+  prog_addrs_t vaddr;
+
+  // make sure the cached info corrosponds to the current prog_handle
+  if (g_cached_prog_handle != prog_handle)
+  {
+    res = loader_GetProgramInfo(&g_exheader, prog_handle);
+    g_cached_prog_handle = prog_handle;
+    if (res < 0)
+    {
+      g_cached_prog_handle = 0;
+      return res;
+    }
+  }
+
+  // get kernel flags
+  flags = 0;
+  for (count = 0; count < 28; count++)
+  {
+    desc = *(u32 *)g_exheader.accessdesc.arm11kernelcaps.descriptors[count];
+    if (0x1FE == desc >> 23)
+    {
+      flags = desc & 0xF00;
+    }
+  }
+  if (flags == 0)
+  {
+    return 0xD8E00402;
+  }
+
+  // allocate process memory
+  vaddr.text_addr = *(u32_t *)g_exheader.codesetinfo.text.address;
+  vaddr.text_size = (*(u32_t *)g_exheader.codesetinfo.text.codesize + 4095) >> 12;
+  vaddr.ro_addr = *(u32_t *)g_exheader.codesetinfo.ro.address;
+  vaddr.ro_size = (*(u32_t *)g_exheader.codesetinfo.ro.codesize + 4095) >> 12;
+  vaddr.data_addr = *(u32_t *)g_exheader.codesetinfo.data.address;
+  vaddr.data_size = (*(u32_t *)g_exheader.codesetinfo.data.codesize + 4095) >> 12;
+  vaddr.total_size = vaddr.text_size + vaddr.ro_size + vaddr.data_size;
+  if ((res = allocate_shared_mem(&shared_addr, &vaddr, flags)) < 0)
+  {
+    return res;
+  }
+
+  // load code
+  if ((res = load_code(&shared_addr, prog_handle, g_exheader.codesetinfo.flags & 1)) >= 0)
+  {
+    memcpy(&codesetinfo.name, g_exheader.codesetinfo.name);
+    codesetinfo.program_id = prog_id;
+    codesetinfo.text_addr = vaddr.text_addr;
+    codesetinfo.text_size = vaddr.text_size;
+    codesetinfo.text_size_total = vaddr.text_size;
+    codesetinfo.ro_addr = vaddr.ro_addr;
+    codesetinfo.ro_size = vaddr.ro_size;
+    codesetinfo.ro_size_total = vaddr.ro_size;
+    codesetinfo.data_addr = vaddr.data_addr;
+    codesetinfo.data_size = vaddr.data_size;
+    codesetinfo.data_size_total = vaddr.data_size;
+    res = svcCreateCodeSet(&codeset, &codesetinfo, shared_addr.text_addr, shared_addr.ro_addr, shared_addr.data_addr);
+    if (res >= 0)
+    {
+      res = svcCreateProcess(process, codeset, &g_exheader.accessdesc.arm11kernelcaps, count);
+      if (res >= 0)
+      {
+        return 0;
+      }
+    }
+  }
+
+  return res;
+}
+
+static int loader_RegisterProgram(u64 *prog_handle, FS_ProgramInfo *title, FS_ProgramInfo *update)
+{
+
+}
+
+static int loader_GetProgramInfo(exheader_header *exheader, u64 prog_handle)
+{
+
+}
+
+static int loader_UnregisterProgram(u64 prog_handle)
+{
+  
+
+}
 
 static void handle_commands(void)
 {
   u32* cmdbuf;
   u16 cmdid;
+  int res;
+  Handle handle;
+  u64 prog_handle;
 
   cmdbuf = getThreadCommandBuffer();
   cmdid = cmdbuf[0] >> 16;
@@ -17,18 +144,51 @@ static void handle_commands(void)
   {
     case 1: // LoadProcess
     {
+      res = loader_LoadProcess(&handle, *(u64 *)&cmdbuf[1]);
+      cmdid[0] = 0x10042;
+      cmdid[1] = res;
+      cmdid[2] = 16;
+      cmdid[3] = handle;
       break;
     }
     case 2: // RegisterProgram
     {
+      res = loader_RegisterProgram(&prog_handle, (FS_ProgramInfo *)&cmdid[1], (FS_ProgramInfo *)&cmdid[5]);
+      cmdid[0] = 0x200C0;
+      cmdid[1] = res;
+      *(u64 *)&cmdid[2] = prog_handle;
       break;
     }
     case 3: // UnregisterProgram
     {
+      if (g_cached_prog_handle == prog_handle)
+      {
+        g_cached_prog_handle = 0;
+      }
+      cmdid[0] = 0x30040;
+      cmdid[1] = loader_UnregisterProgram(*(u64 *)&cmdbuf[1]);
       break;
     }
     case 4: // GetProgramInfo
     {
+      prog_handle = *(u64 *)&cmdid[1];
+      if (prog_handle != g_cached_prog_handle)
+      {
+        res = loader_GetProgramInfo(&g_exheader, prog_handle);
+        if (res >= 0)
+        {
+          g_cached_prog_handle = prog_handle;
+        }
+        else
+        {
+          g_cached_prog_handle = 0;
+        }
+      }
+      memcpy(&g_ret_buf, &g_exheader, 1024);
+      cmdid[0] = 0x40042;
+      cmdid[1] = res;
+      cmdid[2] = 0x1000002;
+      cmdid[3] = &g_ret_buf;
       break;
     }
     default: // error
@@ -68,6 +228,7 @@ int main(int argc, const char *argv[])
   }
 
   g_active_handles = 2;
+  g_cached_prog_handle = 0;
   index = 1;
 
   reply_target = 0;
