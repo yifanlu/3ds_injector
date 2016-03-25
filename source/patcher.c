f824b684736184b2abb7ca2312adc0f6727a5671
Yifan Lu <me@yifanlu.com>
2016-03-25 00:45:52 -0500

    Add SDMC access

--- source/patcher.c
+++ source/patcher.c
@@ -1,5 +1,38 @@
 #include <3ds.h>
 #include "patcher.h"
+#include "ifile.h"
+
+#ifndef PATH_MAX
+#define PATH_MAX 255
+#endif
+
+static int open_file(IFile *file, const char *path, int flags)
+{
+  FS_Archive archive;
+  FS_Path ppath;
+  u16 wpath[PATH_MAX+1];
+  size_t len;
+  size_t i;
+
+  len = strnlen(path, PATH_MAX);
+  if (len >= PATH_MAX)
+  {
+    return -1;
+  }
+
+  for (i = 0; i < len; i++)
+  {
+    wpath[i] = path[i];
+  }
+  wpath[i] = 0;
+  archive.id = ARCHIVE_SDMC;
+  archive.lowPath.type = PATH_EMPTY;
+  archive.lowPath.size = 0;
+  ppath.type = PATH_UTF16;
+  ppath.data = wpath;
+  ppath.size = 2 * (len + 1);
+  return IFile_Open(file, archive, ppath, flags);
+}
 
 int patch_code(u64 progid, u8 *code, u32 size)
 {
